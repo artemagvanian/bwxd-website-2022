9e46cc5081c023b50544daa1a8dbe7e3938a30e3
Artem Agvanian <artemagvanian@icloud.com>
2021-12-31 14:38:01 +0100

    create minimal project structure

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-console.log("Hello!");
+import { Routes, Route, Link } from "react-router-dom";
+import { Home } from "./views/Home";
+import { About } from "./views/About";
+
+export function App() {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
