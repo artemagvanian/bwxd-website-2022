73c1b17c46bed18da7eb89a092f78dc4ff8929c0
aanchalsheth31 <asheth@betterworldxdesign.com>
2022-04-06 19:15:15 -0400

    added fonts and started sponsor

--- src/views/Sponsors.js
+++ src/views/Sponsors.js
@@ -1,3 +1,28 @@
+import * as _ from "lodash";
+//import React, { useState, useEffect } from "react";
+import { Grid, Cell, Triangle } from "../components/Grid";
+import * as styles from "./Sponsors.module.css";
+
 export function Sponsors() {
-  return <h1>Sponsors</h1>;
+  return (
+    <div>
+      {" "}
+      <h1>sponsors</h1>
+      <Grid>
+        {/* {_.times(15, (i) => (
+        <Triangle key={i} className={styles.homeTriangle} />
+      ))} */}
+        {_.times(15, (i) => (
+          <Cell key={i} className={styles.homeCell} />
+        ))}
+        {_.times(15, (i) => (
+          <Cell key={i} className={styles.homeCell} />
+        ))}
+        {_.times(2, (i) => (
+          <Cell key={i} className={styles.homeCell} />
+        ))}
+      </Grid>
+    </div>
+  );
 }
+export default Sponsors;
