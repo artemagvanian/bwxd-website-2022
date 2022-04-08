a7395e19daeb4d7723450bcc656e46b575601060
Artem Agvanian <artemagvanian@icloud.com>
2022-04-08 11:15:10 -0400

    Implement more of homepage

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,4 +1,5 @@
 import * as _ from "lodash";
+import Spline from "@splinetool/react-spline";
 import React, { useState, useEffect } from "react";
 import { Grid, Cell, Triangle } from "../components/Grid";
 import * as styles from "./Home.module.scss";
@@ -56,85 +57,96 @@ function NBlueCell({ n }) {
 
 export function Home() {
   return (
-    <Grid>
-      {/* row */}
-      <NHomeCell n={15} />
-      {/* row */}
-      <div className={styles.whyReframe}>
-        <h1>Why Reframe?!!</h1>
+    <>
+      <Grid>
+        {/* row */}
+        <NHomeCell n={15} />
+        {/* row */}
+        <div className={styles.whyReframe}>
+          <h2>Why Reframe?</h2>
+        </div>
+        <NHomeCell n={3} />
+        {/* row */}
+        <DecorativeFigure className={styles.decorativeFigure} />
+        <div className={styles.goal}>
+          <p>
+            Our goal is to reframe the way we see design, and through design,
+            reframe interventions in the world's contemporary issues. We've
+            chosen Re-Frame for this year's theme as an opportunity to highlight
+            overlooked narratives and initiate change through new perspectives.
+          </p>
+        </div>
+        <Triangle className={styles.bigTriangle} />
+        <Cell className={styles.bigCell} />
+        <NHomeCell n={6} />
+        {/* row */}
+        <NHomeCell n={1} />
+        <NBlueCell n={2} />
+        <div className={styles.context}>
+          <p>
+            Through the context of the “frame,” we consider how design and
+            social practices can challenge traditional boundaries.
+          </p>
+        </div>
+        <NHomeCell n={1} />
+        <NBlueCell n={2} />
+        <NHomeCell n={1} />
+        <NBlueCell n={2} />
+        <NHomeCell n={1} />
+        <NBlueCell n={2} />
+        {/* row */}
+        <NHomeCell n={1} />
+        <NBlueCell n={2} />
+        <NHomeCell n={12} />
+        {/* row */}
+        <NHomeCell n={1} />
+        <NBlueCell n={1} />
+        <div className={styles.how}>
+          <h2>How can we reframe use, purpose, and understanding?</h2>
+          <p>
+            We recognize the power in recontextualizing our understanding of
+            preexisting systems and methodologies. <br /> <br />
+            Our goal is to generate discussion around what frames enclose and
+            exclude, and examine how voices can be pulled to the forefront
+            through collaborative problem solving. We hope to explore practices
+            that challenge and disrupt traditional conventions, amplify
+            historically underrepresented voices, and draw attention to local
+            and global inequalities. To drive our exploration, we bring together
+            a diverse community of critical thinkers.
+          </p>
+        </div>
+        <NHomeCell n={6} />
+        <NBlueCell n={1} />
+        <NHomeCell n={6} />
+        <NBlueCell n={1} />
+        <NHomeCell n={6} />
+        <NBlueCell n={1} />
+        <NHomeCell n={6} />
+        <NBlueCell n={1} />
+        <NHomeCell n={6} />
+        <NBlueCell n={2} />
+        <NHomeCell n={4} />
+        <div className={styles.applications}>
+          <p>
+            Re-framing can be applied at many different levels of design
+            practice. We have identified a few frameworks through which reframe
+            can be understood in relation to your work. This is not meant to
+            inhibit you, but rather to initiate a thought-provoking connection
+            between your work and the conference's theme.
+          </p>
+        </div>
+        <NHomeCell n={9} />
+      </Grid>
+      <div className={styles.splineContainer}>
+        <Spline
+          className={styles.spline}
+          scene="https://prod.spline.design/b1UMZ0OW-nbQrlY6/scene.spline"
+        />
+        <Grid>
+          <NHomeCell n={120} />
+        </Grid>
       </div>
-      <NHomeCell n={3} />
-      {/* row */}
-      <DecorativeFigure className={styles.decorativeFigure} />
-      <div className={styles.goal}>
-        <p>
-          Our goal is to reframe the way we see design, and through design,
-          reframe interventions in the world's contemporary issues. We've chosen
-          Re-Frame for this year's theme as an opportunity to highlight
-          overlooked narratives and initiate change through new perspectives.
-        </p>
-      </div>
-      <Triangle className={styles.bigTriangle} />
-      <Cell className={styles.bigCell} />
-      <NHomeCell n={6} />
-      {/* row */}
-      <NHomeCell n={1} />
-      <NBlueCell n={2} />
-      <div className={styles.context}>
-        <p>
-          Through the context of the “frame,” we consider how design and social
-          practices can challenge traditional boundaries.
-        </p>
-      </div>
-      <NHomeCell n={1} />
-      <NBlueCell n={2} />
-      <NHomeCell n={1} />
-      <NBlueCell n={2} />
-      <NHomeCell n={1} />
-      <NBlueCell n={2} />
-      {/* row */}
-      <NHomeCell n={1} />
-      <NBlueCell n={2} />
-      <NHomeCell n={12} />
-      {/* row */}
-      <NHomeCell n={1} />
-      <NBlueCell n={1} />
-      <div className={styles.how}>
-        <h2>How can we reframe use, purpose, and understanding?</h2>
-        <p>
-          We recognize the power in recontextualizing our understanding of
-          preexisting systems and methodologies. <br />
-          Our goal is to generate discussion around what frames enclose and
-          exclude, and examine how voices can be pulled to the forefront through
-          collaborative problem solving. We hope to explore practices that
-          challenge and disrupt traditional conventions, amplify historically
-          underrepresented voices, and draw attention to local and global
-          inequalities. To drive our exploration, we bring together a diverse
-          community of critical thinkers.
-        </p>
-      </div>
-      <NHomeCell n={6} />
-      <NBlueCell n={1} />
-      <NHomeCell n={6} />
-      <NBlueCell n={1} />
-      <NHomeCell n={6} />
-      <NBlueCell n={1} />
-      <NHomeCell n={6} />
-      <NBlueCell n={1} />
-      <NHomeCell n={6} />
-      <NBlueCell n={2} />
-      <NHomeCell n={4} />
-      <div className={styles.applications}>
-        <p>
-          Re-framing can be applied at many different levels of design practice.
-          We have identified a few frameworks through which reframe can be
-          understood in relation to your work. This is not meant to inhibit you,
-          but rather to initiate a thought-provoking connection between your
-          work and the conference's theme.
-        </p>
-      </div>
-      <NHomeCell n={9} />
-    </Grid>
+    </>
   );
 }
 
