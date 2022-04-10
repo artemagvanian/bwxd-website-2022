de07e0b67d83cc23f9e4cdce766405a6b3195104
aanchalsheth31 <asheth@betterworldxdesign.com>
2022-04-10 16:18:57 -0400

    sponsors rough

--- src/views/Sponsors.js
+++ src/views/Sponsors.js
@@ -1,14 +1,24 @@
 import * as _ from "lodash";
 //import React, { useState, useEffect } from "react";
 import { Grid, Cell, Triangle } from "../components/Grid";
-import * as styles from "./Sponsors.module.css";
+import * as styles from "./Sponsors.module.scss";
+
+function NHomeCell({ n }) {
+  return (
+    <>
+      {_.times(n, (i) => (
+        <Cell key={i} className={styles.homeCell} />
+      ))}
+    </>
+  );
+}
 
 export function Sponsors() {
   return (
     <div>
       {" "}
       <Grid>
-        {_.times(15, (i) => (
+        {/* {_.times(15, (i) => (
           <Cell key={i} className={styles.homeCell} />
         ))}
         {_.times(15, (i) => (
@@ -24,7 +34,71 @@ export function Sponsors() {
       <Grid>
         {_.times(1, (i) => (
           <Cell key={i} className={styles.homeCell} />
-        ))}
+        ))} */}
+        <NHomeCell n={15} />
+        <NHomeCell n={15} />
+        {/* sandwich ways of adding boxes */}
+        <NHomeCell n={1} />
+        <div className={styles.mainHeading}>
+          <h1>Sponsors</h1>
+        </div>
+        <NHomeCell n={1} />
+        {/* sandwich ways of adding boxes */}
+        <NHomeCell n={15} />
+        <div className={styles.subHeading}>
+          <h2>Why Sponsor Us?</h2>
+        </div>
+        <div className={styles.filledBoxOne}></div>
+        <NHomeCell n={12} />
+        <div className={styles.emptyBoxOne}></div>
+        <div className={styles.imageOne}></div>
+        <div className={styles.connect}>
+          <h2>Connect</h2>
+          <p>
+            With talented designers, artists, engineers, and entrepreneurs from
+            across the country and around the world.
+          </p>
+        </div>
+        <NHomeCell n={3} />
+        <NHomeCell n={3} />
+        <div className={styles.influence}>
+          <h2>Influence</h2>
+          <p>And work with innovators for creative business solutions.</p>
+        </div>
+        <NHomeCell n={3} />
+        <div className={styles.recruit}>
+          <h2>Recruit</h2>
+          <p>Students from Brown, RISD, and other top-tier universities.</p>
+        </div>
+        <div className={styles.imageTwo}></div>
+
+        <div className={styles.emptyBoxTwo}></div>
+        <NHomeCell n={2} />
+        <NHomeCell n={2} />
+        <div className={styles.filledBoxTwo}></div>
+        <div className={styles.establish}>
+          <h2>Establish</h2>
+          <p>A lasting presence with future change-makers in the Northeast.</p>
+        </div>
+        <NHomeCell n={7} />
+        <NHomeCell n={7} />
+        <NHomeCell n={15} />
+        <NHomeCell n={2} />
+        <div className={styles.prevPartners}>
+          <h2>Previous Partnersss</h2>
+        </div>
+        {/* some issue!! */}
+        <div className={styles.imageThree}></div>
+        <NHomeCell n={2} />
+        <NHomeCell n={2} />
+        <NHomeCell n={2} />
+        <NHomeCell n={2} />
+        <NHomeCell n={2} />
+        <NHomeCell n={2} />
+        <NHomeCell n={15} />
+
+        {/* <div className={styles.filledBoxThree}></div>
+        <NHomeCell n={15} /> */}
       </Grid>
     </div>
   );
