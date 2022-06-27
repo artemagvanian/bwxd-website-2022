0bbde4e9780a5b545c82ea2e169b7cb714cf93b2
Artem Agvanian <artemagvanian@icloud.com>
2022-06-27 21:04:04 +0200

    Implement mobile version of the home page

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,13 +1,19 @@
 import Spline from "@splinetool/react-spline";
 import { times } from "lodash";
 
-import { Grid, Cell, Triangle } from "../components/Grid";
+import {
+  Grid,
+  Cell,
+  Triangle,
+  MobileGrid,
+  DiagonalCell,
+} from "../components/Grid";
 import * as styles from "./Home.module.scss";
+import * as mobileStyles from "./Home.mobile.module.scss";
 
 import { BREAKPOINT } from "../App";
 
 const hero = new URL("../assets/hero.svg", import.meta.url);
-const mobileHero = new URL("../assets/hero_mobile.svg", import.meta.url);
 
 function DecorativeFigure({ className }) {
   return (
@@ -60,7 +66,31 @@ function NBlueCell({ n }) {
   );
 }
 
-export function Hero() {
+function TriangleIcon({ className }) {
+  return (
+    <svg className={className} viewBox="0 0 100 100">
+      <polygon points="50,20 15,80 85,80" fill="#2d2ff5" />
+    </svg>
+  );
+}
+
+function CircleIcon({ className }) {
+  return (
+    <svg className={className} viewBox="0 0 100 100">
+      <ellipse cx="50" cy="50" rx="30" ry="30" fill="#2d2ff5" />
+    </svg>
+  );
+}
+
+function SquareIcon({ className }) {
+  return (
+    <svg className={className} viewBox="0 0 100 100">
+      <polygon points="20,20 80,20 80,80 20,80" fill="#2d2ff5" />
+    </svg>
+  );
+}
+
+function Hero() {
   function onLoad(spline) {
     const zoom = 0.9 / 1000;
     spline.setZoom(zoom * window.innerWidth);
@@ -78,26 +108,90 @@ export function Hero() {
   );
 }
 
-export function MobileHero() {
+function MobileHero() {
+  return <object className={styles.hero} data={hero} type="image/svg+xml" />;
+}
+
+function MobileHome() {
   return (
-    <object
-      className={styles.mobileHero}
-      data={mobileHero}
-      type="image/svg+xml"
-    />
+    <>
+      <MobileHero />
+      <MobileGrid>
+        <NHomeCell n={1} />
+        <div className={mobileStyles.goal}>
+          <p>
+            Our goal is to reframe the way we see design, and through design,
+            reframe interventions in the world's contemporary issues. We've
+            chosen Re-Frame for this year's theme as an opportunity to highlight
+            overlooked narratives and initiate change through new perspectives.
+          </p>
+        </div>
+        <div className={mobileStyles.whyReframe}>
+          <h2>Why Re-Frame?</h2>
+        </div>
+        <NHomeCell n={17} />
+        <DiagonalCell className={mobileStyles.diagonalCell} />
+        <Cell className={mobileStyles.longCell} />
+        <Triangle className={mobileStyles.smallTriangle} />
+        <Cell className={mobileStyles.blueCell} />
+        <NHomeCell n={2} />
+        <div className={mobileStyles.context}>
+          <h2>
+            How can design and social practices challenge traditional
+            boundaries.
+          </h2>
+        </div>
+        <NHomeCell n={6} />
+        <Cell className={mobileStyles.bigCell} />
+        <div className={mobileStyles.use}>
+          <CircleIcon />
+          <h2>Use</h2>
+        </div>
+        <NHomeCell n={3} />
+        <div className={mobileStyles.purpose}>
+          <SquareIcon />
+          <h2>Purpose</h2>
+        </div>
+        <NHomeCell n={2} />
+        <div className={mobileStyles.understanding}>
+          <TriangleIcon />
+          <h2>Understanding</h2>
+        </div>
+        <NHomeCell n={9} />
+        <div className={mobileStyles.power}>
+          <p>
+            We recognize the power in recontextualizing our understanding of
+            preexisting systems and methodologies. <br /> <br />
+            Our goal is to generate discussion around what frames enclose and
+            exclude, and examine how voices can be pulled to the forefront
+            through collaborative problem solving. We hope to explore practices
+            that challenge and disrupt traditional conventions, amplify
+            historically underrepresented voices, and draw attention to local
+            and global inequalities. To drive our exploration, we bring together
+            a diverse community of critical thinkers.
+          </p>
+        </div>
+        <NBlueCell n={8} />
+      </MobileGrid>
+      <div className={mobileStyles.stepOutContainer}>
+        <div className={mobileStyles.stepOutElement}>
+          <h2>How do you step outside of the "frame"?</h2>
+        </div>
+        <MobileGrid>
+          <NHomeCell n={45} />
+        </MobileGrid>
+      </div>
+    </>
   );
 }
 
-export function Home() {
-  const width = window.innerWidth;
-
+function DesktopHome() {
   function onTextLoad(spline) {
     const zoom = 0.75 / 1000;
     spline.setZoom(zoom * window.innerWidth);
   }
-  return width < BREAKPOINT ? (
-    <MobileHero />
-  ) : (
+
+  return (
     <>
       <Hero />
       <Grid>
@@ -186,6 +280,11 @@ export function Home() {
       />
     </>
   );
+}
+
+export function Home() {
+  const width = window.innerWidth;
+  return width < BREAKPOINT ? <MobileHome /> : <DesktopHome />;
 }
 
 export default Home;
